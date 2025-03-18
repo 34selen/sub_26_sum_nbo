882875d97e52e4f6d653fea2d3834a9551a02f73
34selen <73600142+34selen@users.noreply.github.com>
2025-03-18 23:16:16 +0900

    UPDATE:file read
    파일 읽고 바이트순서 변환까지 완료

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <stddef.h> // for size_t
-#include <stdint.h> // for uint8_t
-#include <stdio.h>  // for printf
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 uint32_t my_ntohl(uint32_t n)
 { // 3412
@@ -14,4 +14,36 @@ uint32_t my_ntohl(uint32_t n)
 }
 int main(int argc, char *argv[])
 {
+    int count = argc - 1;
+    uint32_t *array = (uint32_t *)calloc(count, sizeof(uint32_t));
+    printf("%d\n", count);
+    for (int i = 1; i <= count; i++)
+    {
+        FILE *file = fopen(argv[i], "rb");
+        if (!file)
+        {
+            printf("Failed to open file");
+            continue;
+        }
+        if (fread(&array[i - 1], sizeof(uint32_t), 1, file) != 1)
+        {
+            printf("Failed to read data");
+            fclose(file);
+            continue;
+        }
+        fclose(file);
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = my_ntohl(array[i]);
+    }
+    for (int i = 0; i < count; i++)
+    {
+        printf("Raw value (Hex): 0x%X\n", array[i]);
+        printf("Raw value (Dec): %d\n", array[i]);
+    }
+
+    free(array);
+    return 0;
 }
